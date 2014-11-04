be4bf00d459c513a38c7818b0ae585aee9606b1a
Joey Roosing <jroosing@qafe.com>
2014-11-04 16:15:29 +0100

    QAFE-1019 As a QAFE Developer I want to generalize the QAFE Business WebService such that it can be sold to other customers - Soap webservice is now available in qafe-platform as a module

--- qafe-webservice/src/main/java/com/qualogy/ws/soap/impl/QafeWebserviceImpl.java
+++ qafe-webservice/src/main/java/com/qualogy/ws/soap/impl/QafeWebserviceImpl.java
@@ -15,11 +15,8 @@
  */
 package com.qualogy.ws.soap.impl;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.Map;
-import java.util.Properties;
 
 import javax.jws.WebService;
 
@@ -27,6 +24,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.qualogy.qafe.bind.core.application.Configuration;
+import com.qualogy.qafe.core.application.ApplicationCluster;
 import com.qualogy.ws.businessaction.BusinessActionExecutor;
 import com.qualogy.ws.businessaction.impl.BusinessActionExecutorImpl;
 import com.qualogy.ws.exception.NotFoundException;
@@ -34,32 +32,19 @@ import com.qualogy.ws.soap.QafeWebservice;
 
 @WebService(endpointInterface = "com.qualogy.ws.soap.QafeWebservice")
 public class QafeWebserviceImpl implements QafeWebservice {
+    
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     private final Gson gson;
     private final BusinessActionExecutor businessActionExecutor = new BusinessActionExecutorImpl();
     final Type typeOf = new TypeToken<Map<String,Object>>(){}.getType();
     
     public QafeWebserviceImpl() {
-        String dateFormat = "yyyy-MM-dd HH:mm:ss";
+                 
+        String dateFormat = ApplicationCluster.getInstance().getConfigurationItem(Configuration.WEB_SERVICE_FORMAT_DATE);
         
-        Properties externalProperties = new Properties();
-        
-        InputStream in = this.getClass().getResourceAsStream("/external.properties");
-        try {
-            externalProperties.load(in);
-            if (externalProperties.containsKey(Configuration.DATE_FORMAT)) {
-                dateFormat = externalProperties.getProperty(Configuration.DATE_FORMAT);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        if (dateFormat == null) {
+            dateFormat = DEFAULT_DATE_FORMAT;
         }
         
         gson = new GsonBuilder().setDateFormat(dateFormat).create();
@@ -68,14 +53,7 @@ public class QafeWebserviceImpl implements QafeWebservice {
 	@Override
 	public String executeBusinessAction(String appId, String businessActionId, String inputAsJson) throws NotFoundException {
 	    Map<String, Object> inputParams = gson.fromJson(inputAsJson, typeOf);
-	    Object output = null;
-        try {
-            output = businessActionExecutor.execute(appId, businessActionId, inputParams);
-        } catch (NotFoundException e) {
-            throw e;
-        }
-	    	    
+	    Object output = businessActionExecutor.execute(appId, businessActionId, inputParams);	    
 		return gson.toJson(output);
 	}
-
 }
