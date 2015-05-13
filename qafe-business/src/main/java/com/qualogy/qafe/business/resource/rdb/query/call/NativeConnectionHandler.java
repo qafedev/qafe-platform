3de20de8e9d6d4ef643c3dde987674582ebf360d
amohabirQAFE <amohabir@qafe.com>
2015-05-13 09:17:03 +0200

    QAFE-1212 [L] As a QAFE ISV I want the JNDI connection to work with Database object types such that on a weblogic or tomcat server the JNDI can be used and datasource pool management can be managed by the customer - Added additional connectors mentioned in the issue. - Small refactoring done for better maintainability.

--- qafe-business/src/main/java/com/qualogy/qafe/business/resource/rdb/query/call/NativeConnectionHandler.java
+++ qafe-business/src/main/java/com/qualogy/qafe/business/resource/rdb/query/call/NativeConnectionHandler.java
@@ -1,43 +1,75 @@
+/**
+ * Copyright 2008-2015 Qualogy Solutions B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.qualogy.qafe.business.resource.rdb.query.call;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
-import org.springframework.jdbc.support.nativejdbc.CommonsDbcpNativeJdbcExtractor;
 import org.springframework.jdbc.support.nativejdbc.NativeJdbcExtractor;
-import org.springframework.jdbc.support.nativejdbc.WebLogicNativeJdbcExtractor;
 
 public class NativeConnectionHandler {
     
     private static final Logger LOG = Logger.getLogger(BaseCall.class.getName());
 
+    private static List<String> possibleConnectionClasses = new ArrayList<String>();
+    
+    static {
+    	possibleConnectionClasses.add("org.springframework.jdbc.support.nativejdbc.WebLogicNativeJdbcExtractor");
+    	possibleConnectionClasses.add("org.springframework.jdbc.support.nativejdbc.CommonsDbcpNativeJdbcExtractor");
+    	possibleConnectionClasses.add("org.springframework.jdbc.support.nativejdbc.C3P0NativeJdbcExtractor");
+    	possibleConnectionClasses.add("org.springframework.jdbc.support.nativejdbc.JBossNativeJdbcExtractor");
+    	possibleConnectionClasses.add("org.springframework.jdbc.support.nativejdbc.WebSphereNativeJdbcExtractor");
+    	possibleConnectionClasses.add("org.springframework.jdbc.support.nativejdbc.XAPoolNativeJdbcExtractor");
+    }
+    
+    public static Connection nativeConnection;
+    
     public static Connection getNativeConnection(Connection conn) throws SQLException {
-        Connection nativeConnection = conn;
-        NativeJdbcExtractor extractor = findNativeConnectionExtractor();
-        LOG.info("Natice Connection Extractor :" + extractor.getClass());
+        // Caching purposes
+    	if (nativeConnection != null) {
+        	return nativeConnection;
+        }
+        
+    	Connection nativeConnection = conn;
+    	NativeJdbcExtractor extractor = findNativeConnectionExtractor();
+        LOG.info("Native Connection Extractor :" + extractor.getClass());
         if (extractor != null) {
             nativeConnection = extractor.getNativeConnection(conn);
         }
-        LOG.info("Natice Connection  :" + nativeConnection.getClass());
+        LOG.info("Native Connection  :" + nativeConnection.getClass());
 
         return nativeConnection;
     }
 
     private static NativeJdbcExtractor findNativeConnectionExtractor() {
         NativeJdbcExtractor extractor = null;
-        try {
-            extractor = new WebLogicNativeJdbcExtractor();
-        } catch (Exception e) {
+        
+        for (final String connectionClass : possibleConnectionClasses) {
+        	try {
+				extractor = (NativeJdbcExtractor) Class.forName(connectionClass).newInstance();
+				LOG.info("Current Extractor successful:" + connectionClass);
+				return extractor;
+			} catch (Exception e) {
+				LOG.info("Current Extractor failed:" + connectionClass);
+			}
         }
 
-        if (extractor != null) {
-            return extractor;
-        }
-        try {
-            extractor = new CommonsDbcpNativeJdbcExtractor();
-        } catch (Exception e) {
-        }
         return extractor;
     }
 }
