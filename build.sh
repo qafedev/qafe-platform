1189930dc97fcf91c2ce131e09f37e629afe94af
Binu Badarudeen <binub@qualogy.com>
2017-05-18 15:42:34 +0200

    QAFE-1235: Fix to avoid passowrd shown to user - Added Jenkins file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-export PATH=$(echo $PATH | sed 's/8/7/g')
-mvn -s settings.xml clean install -Dsolutions.codechecking.phase=verify
+echo 'test'
+#export PATH=$(echo $PATH | sed 's/8/7/g')
+#mvn -s settings.xml clean install -Dsolutions.codechecking.phase=verify
